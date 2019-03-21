9d7bb78b3b76a4a2b27bfe492bef054c40ee2931
Troy Gooden <tro12185@gmail.com>
2019-03-21 05:05:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length <= 2 
     array.join(" and ")
   elsif array.length > 2
-     array[0...array.length - 1].join(", ") +  ", and #{array.last}"
+     array[0..array.length - 1].join(", ") +  ", and #{array.last}"
   end
 end
 
