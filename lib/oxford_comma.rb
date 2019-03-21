5b8fed409ce6bf32f978266734384d51d0ead83c
Troy Gooden <tro12185@gmail.com>
2019-03-21 04:57:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  if array.length <= 2
-    array.join(" and ")
-  end 
+  if array.length > 2
+    tooki = darray[0...darray.length - 1].join(", ")
+  end
+    tooki += ", and #{darray.last}"
 end
 
