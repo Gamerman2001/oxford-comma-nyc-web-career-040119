c7bb02e95b24ccf4ef5921f4a6afc0f655846fd2
Troy Gooden <tro12185@gmail.com>
2019-03-21 04:58:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length > 2
-    tooki = darray[0...darray.length - 1].join(", ")
+    tooki = array[0...darray.length - 1].join(", ")
   end
     tooki += ", and #{darray.last}"
 end
