4d1a8f5e6b5d05e69a497d2ff833d3107c072d1f
Troy Gooden <tro12185@gmail.com>
2019-03-21 05:02:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length <= 2 
     array.join("and")
   elsif array.length > 2
-    tooki = array[0...array.length - 1].join(", ")
+     array[0...array.length - 1].join(", ")
   end
-    tooki += ", and #{array.last}"
+    array+= ", and #{array.last}"
 end
 
